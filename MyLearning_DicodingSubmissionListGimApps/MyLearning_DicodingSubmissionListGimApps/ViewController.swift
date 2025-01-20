7289723b71f856eb4c71a5f14c60bf44fcdf3ab0
Git Ignore <telkom@telkoms-MacBook-Pro-2.local>
2025-01-20 11:47:27 +0700

    Initial commit

---
+++ MyLearning_DicodingSubmissionListGimApps/MyLearning_DicodingSubmissionListGimApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLearning_DicodingSubmissionListGimApps
+//
+//  Created by telkom on 20/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
