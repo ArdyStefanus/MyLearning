4f1160a7a0b21dce861aedbb76add1acdb6f4b03
Git Ignore <telkom@telkoms-MacBook-Pro-2.local>
2025-01-21 14:18:20 +0700

    create code base

--- MyLearning_DicodingSubmissionListGimApps/MyLearning_DicodingSubmissionListGimApps/Helper/Constant.swift
+++ MyLearning_DicodingSubmissionListGimApps/MyLearning_DicodingSubmissionListGimApps/Helper/Constant.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+struct NibFile {
+    static let HOME_TABBAR = "HomeTabBarController"
+    
+    // MARK: DETAIL LIST GIM
+    static let DETAIL_LIST_GIM = "DetailGimVC"
+    static let DETAIL_LIST_GIM_FAVORIT = "DetailFavoritVC"
+    
+    // MARK: CELL LIST GAME
+    static let LIST_GAME_CELL = "ListGameCell"
+}
