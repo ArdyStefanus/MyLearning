a0b3ac2059560c4dd6eb81ab7136cf3406c2ed30
Git Ignore <telkom@telkoms-MacBook-Pro-2.local>
2025-01-21 14:17:45 +0700

    create code base

---
+++ MyLearning_DicodingSubmissionListGimApps/MyLearning_DicodingSubmissionListGimApps/Helper/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  MyLearning_DicodingSubmissionListGimApps
+//
+//  Created by telkom on 21/01/25.
+//
+
+import Foundation
---
+++ MyLearning_DicodingSubmissionListGimApps/MyLearning_DicodingSubmissionListGimApps/HomeTabBar/HomeTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTabBarController.swift
+//  MyLearning_DicodingSubmissionListGimApps
+//
+//  Created by telkom on 21/01/25.
+//
+
+import UIKit
+
+class HomeTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
